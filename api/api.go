ce55c22b504129d11f901a83b32d07ff5badc3d7
DuongHieuMAI <maiduonghieu@gmail.com>
2021-05-10 01:01:53 +0700

    change path

--- api/api.go
+++ api/api.go
@@ -1,8 +1,8 @@
 package goofys
 
 import (
-	. "github.com/kahing/goofys/api/common"
-	"github.com/kahing/goofys/internal"
+	. "github.com/DuongHieuMAI/goofys/api/common"
+	"github.com/DuongHieuMAI/goofys/internal"
 
 	"context"
 	"fmt"
